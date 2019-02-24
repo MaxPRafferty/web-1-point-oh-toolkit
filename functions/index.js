0ff1bc69dcc1c942f4c8c75bcae71d1831d8b985
MaxPRafferty <max.rafferty@breadfinance.com>
2019-02-23 19:00:38 -0500

    cleaned up, switched ref to query

--- functions/index.js
+++ functions/index.js
@@ -6,48 +6,11 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-exports.addMessage = functions.https.onRequest((req, res) => {
-  try {
-    //const path = req.originalUrl;
-    let docRef = db.collection('counters').doc('website');
-    var setAda = docRef.set({
-      count: 1,
-    });
-  } catch (err) {
-    res.send(JSON.stringify(err));
-    throw err;
-  }
-  res.send('ook');
-});
-
-exports.addMessageTwo = functions.https.onRequest((req, res) => {
-  try {
-    var aTuringRef = db.collection('users').doc('aturing');
-
-    var setAlan = aTuringRef.set({
-      first: 'Alan',
-      middle: 'Mathison',
-      last: 'Turing',
-      born: 1912,
-    });
-  } catch (err) {
-    res.send(JSON.stringify(err));
-    throw err;
-  }
-  res.send('0ook');
-});
-
 exports['counter'] = functions.https.onRequest((req, res) => {
   db.collection('counters')
     .get()
     .then((snapshot) => {
-      let ref = req.header('Referer');
+      let ref = req.query.counter;
       ref = ref || 'root';
       let aTuringRef = db.collection('counters').doc(ref);
       let found;
